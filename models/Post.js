219af02d594b30d58eccb68df9f0caef962356a3
bengamer2005 <157758168+bengamer2005@users.noreply.github.com>
2025-03-08 22:05:43 -0600

    Update Post.js

--- models/Post.js
+++ models/Post.js
@@ -1,15 +1,17 @@
-const mongoose = require('mongoose');
-const User = require('./User')
+const mongoose = require("mongoose");
+const User = require("./User");
 
 const PostSchema = new mongoose.Schema({
-    title: { type: String, required: true },
+    title: {
+        type: String,
+        required: true
+    },
     content: String,
     user: {
-        type: mongoose.Schema.Types.ObjectId, // Asegura que sea ObjectId
-        ref: "User", // Asegura que referencia a la colección 'users'
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
         required: true
     }
 });
 
-
-module.exports = mongoose.model('Post', PostSchema);
+module.exports = mongoose.model("Post", PostSchema);
