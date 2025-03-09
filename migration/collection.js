cf5a3db77076fc56cdbe7642526ea2ac16b09491
bengamer2005 <157758168+bengamer2005@users.noreply.github.com>
2025-03-08 22:06:17 -0600

    Update collection.js

--- migration/collection.js
+++ migration/collection.js
@@ -1,40 +1,34 @@
-// Nota: Este archivo está pensado para ser ejecutado con herramientas de migración como migrate-mongo
 module.exports = {
-    async up(db, client) {
-      // Crear la colección "users" con un validador de esquema
+  async up(db, client) {
       await db.createCollection("users", {
-        validator: {
-          $jsonSchema: {
-            bsonType: "object",
-            required: ["name", "email"],
-            properties: {
-              name: { bsonType: "string" },
-              email: { bsonType: "string" }
-            }
+          validator: {
+              $jsonSchema: {
+                  bsonType: "object",
+                  required: ["name", "email"],
+                  properties: {
+                      name: { bsonType: "string" },
+                      email: { bsonType: "string" }
+                  }
+              }
           }
-        }
       });
-  
-      // Crear la colección "posts" con validación, simulando la existencia de una foreign key
+
       await db.createCollection("posts", {
-        validator: {
-          $jsonSchema: {
-            bsonType: "object",
-            required: ["title", "user"],
-            properties: {
-              title: { bsonType: "string" },
-              content: { bsonType: "string" },
-              // En este caso, se espera que "user" sea un ObjectId que referencie a un usuario
-              user: { bsonType: "objectId" }
-            }
+          validator: {
+              $jsonSchema: {
+                  bsonType: "object",
+                  required: ["title", "user"],
+                  properties: {
+                      title: { bsonType: "string" },
+                      content: { bsonType: "string" },
+                      user: { bsonType: "objectId" }
+                  }
+              }
           }
-        }
       });
-    },
-    async down(db, client) {
-      // Eliminar las colecciones en caso de hacer rollback
+  },
+  async down(db, client) {
       await db.collection("posts").drop();
       await db.collection("users").drop();
-    }
-  };
-  
+  }
+};
